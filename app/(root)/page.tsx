88f91f9985ace6e111b4b7d7c4b723ec50dda012
Kshitij Bhardwaj <tothemoon202154@outlook.com>
2023-12-22 23:56:00 +0530

    conifigured clerk js auth and completed the modals ui

---
+++ app/(root)/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { use, useEffect } from "react";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+const SetupPage = () => {
+
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if(!isOpen){
+      onOpen();
+    }
+  }, [isOpen,onOpen]);
+
+
+
+  return( <div className="p-4">Root Page</div>;)
+};
+
+export default SetupPage;
